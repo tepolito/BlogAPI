b0a6c9ea07dd859836087b2a583ca48c0ca8df8f
TEpolito <tepolito55@gmail.com>
2017-11-29 14:41:46 -0500

    set up server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const blogPostRouter = require('./blogPostRouter');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.use('/blog-posts', blogPostRouter);
+
+app.listen(process.env.PORT || 8080, () => 
+{
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
